1d8cbfb0ff208fe766b62a7b24b7d2f373883ff9
Herculean17 <76151703+jinang17@users.noreply.github.com>
2022-09-11 19:21:18 +0530

    Create Task_model.js

---
+++ models/Task_model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+
+const TaskSchema = new mongoose.Schema({
+  Task_description: {
+    type: String,
+    required: true
+  },
+  Task_completed: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+});
+
+module.exports = mongoose.model("task_collection", TaskSchema);
