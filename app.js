28ad8708a0e1301ee9f6bca0e70ed73d60d76d9a
Herculean17 <76151703+jinang17@users.noreply.github.com>
2023-06-24 23:45:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const url = "mongodb://localhost:27017/TODO_LIST";
-const Task_controller = require("../CRUD_Using_Nodejs/controllers/Task_contoller.js");
+const Task_controller = require("../Done/controllers/Task_controller");
 
 const app = express();
 
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   if (!err) {
-    console.log("Connected to database");
+    console.log("Wooo...Hooo...Connected to database");
   } else {
     console.log("Error in connecting to database " + err);
   }
@@ -19,8 +20,18 @@ app.use(express.json());
 app.use("/", Task_controller);
 
 con.on("open", () => {
-  console.log("Woo...hoooo....wooooho..ho.ho");
-});
-app.listen(5050, () => {
-  console.log("Server is up and walking....");
+  console.log("Connected to MongoDB");
+
+  // Serve static files from the "public" directory
+  app.use(express.static(path.join(__dirname, "public")));
+
+  // Catch-all route to serve the index.html file
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
+  // Start the server
+  app.listen(5050, () => {
+    console.log("Server is up and running on http://localhost:5050");
+  });
 });
